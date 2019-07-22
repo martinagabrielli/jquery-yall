28fc25fac45d720b6a0671890b6a2d45507a520e
Martina Gabrielli <gabrina.martelli@gmail.com>
2019-07-22 13:48:07 +0100

    created rollover effect on an image

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 $(document).ready(function () {
 
-    $('#content-link').click(function () {
+    $('#content-link').click(function() {
         $('#content').toggle();
     });
 
     $('.product-image img').bind({
-        mouseenter : function () {
+        mouseenter : function() {
             var toolTip = $ (this).attr("title");
             $('.info').after('<p class="toolTip">'+toolTip+'</p>');   
         },
-        mouseleave : function () {
+        mouseleave : function() {
             $('p.toolTip').hide();
         }
     });
+
+    $('.product').bind({
+        mousedown : function() {
+            $(this).css('border','3px solid red');
+        },
+        mouseup: function() {
+            $(this).css('border', '3px solid #ccc');
+            $('.cart').append('Apple iPhone 4 is now in the cart<br>');
+            $('.cart h2').text('Shopping Cart contains 1 item!');
+            $(this).hide();
+        }
+    });
+
+    $('.compass').hover(
+        function() {$(this).attr('src', 'img/compass.png');},
+        function() {$(this).attr('src', 'img/compass-bw.png');}
+        );
 });
