7ccbddd2e2560bf6ce294ea940124b78bb49f79f
Martina Gabrielli <gabrina.martelli@gmail.com>
2019-07-22 12:32:45 +0100

    created tooltip that show content during hover event

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
+
     $('#content-link').click(function () {
         $('#content').toggle();
+    });
+
+    $('.product-image img').bind({
+        mouseenter : function () {
+            var toolTip = $ (this).attr("title");
+            $('.info').after('<p class="toolTip">'+toolTip+'</p>');   
+        },
+        mouseleave : function () {
+            $('p.toolTip').hide();
+        }
     });
 });
