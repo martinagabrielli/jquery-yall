93ca2f490ffa98265f548ea4783a7c40cfb8f8f9
Martina Gabrielli <gabrina.martelli@gmail.com>
2019-07-25 12:11:09 +0100

    more jquery effects

--- script.js
+++ script.js
@@ -34,4 +34,76 @@ $(document).ready(function () {
     $('first-name').bind('focus', function(){
         $(this).css('border', '1px solid red');
     });
-});
+    // MAKE A REMAINING CHARACTER SCRIPT
+    $('#status').bind({
+        keypress : function(){
+            var maxNum = 100;
+            var inputText = $(this).val();
+            var numChar = inputText.length;
+            var charRemain = numChar - maxNum;
+            if (numChar <= maxNum) {
+                $('.counter').text(charRemain);
+            }
+            else if (numChar > maxNum) {
+                event.preventDefault();
+            }
+        }
+    });
+    // SHOWING AND HIDING ELEMENTS USING SHOW AND HIDE
+    $('.recipe-name').bind('click', function(){
+        $('.recipe').show();
+    });
+    // one or the other or use .toggle()
+    $('.recipe-name').bind('click', function(){
+        $('.recipe').hide();
+    });
+    // SETTING A MESSAGE TO APPEAR ONLY ONCE ON SITE USING THE SHOW METHOD AND COOKIES
+    $('.special-offer').bind('click', function(){
+        $('#message').show(hideMessage);
+    });
+    $('.hide').bind('click', function(){
+        $('#message').hide(hideMessage);
+    });
+    function hideMessage() {
+        var expirDate=new Date();
+        expirDate.setDate(expirDate.getDate()+30);
+        document.cookie = "name=hideCookie;expires="+expirDate.toUTCString();
+    };
+    var messageCookie = document.cookie;
+    if (messageCookie) {
+        // if message cookie is present, then hide special offer link
+        $('.special-offer').hide();
+    }
+    else {
+        // do nothing
+    }
+    // DISPLAYING ALTERNATIVE SEARCH OPTIONS WITH THE SLIDETOGGLE METHOD
+    $('.advanced-search').bind('click', function(){
+        $('.advanced').slideToggle();
+    });
+    // BUILDING A BASIC IMAGE GALLERY WITH A FADETRANSITION
+    var slideArray = [
+        "pic1.jpg",
+        "pic2.jpg",
+        "pic3.jpg",
+        "pic4.jpg",
+        "pic5.jpg"
+    ];
+    var imgDir = 'img/pic';
+    $('.container').append('<div class="slide-image"></div>');
+    $('.slide-image').html('<img src="img/'+slideArray[0]+'">');
+    $('.slide-image').after('<ul id="nav"></ul>');
+    var slideLength = slideArray.length;
+    for(i=0; i < slideLength; i++) {
+        var slideText = i + 1;
+        $('#nav').append('<li><a href="#" rel="'+slideText+'">'+slideText+'</a></li>');
+    };
+    $('#nav li a').bind('click', function(){
+        var numSlide = $(this).attr('rel');
+        $('.slide-image').html('<img src="'+imgDir + numSlide +'.jpg">');
+        $('.slide-image img').fadeIn();
+        $('#nav li a').removeClass('active');
+        $(this).addClass('active');
+    });
+    $('#nav li a').eq(0).click();
+})
